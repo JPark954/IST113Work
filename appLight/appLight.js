0d8bceb409186427dc997543d42c4f86172080f2
JPark954 <31904700+JPark954@users.noreply.github.com>
2017-10-24 21:24:06 -0400

    changes

--- appLight/appLight.js
+++ appLight/appLight.js
@@ -1,8 +1,7 @@
-document.getElementById("#slider").onclick = myFunction;
+
 function myFunction()
 {
-	document.getElementById("#slider").style.cssFloat="right";
-	document.getElementById("#slider").style.cssFloat="left";
-	document.getElementById("#body").style.color="white";
+	document.getElementById("slider").style.cssFloat="right";
+
 	
 }
