d201580b03021ec1a387fc01f8b5bd3317510c37
JPark954 <31904700+JPark954@users.noreply.github.com>
2017-10-27 13:46:09 -0400

    simpilfied css

--- appLight/appLight.js
+++ appLight/appLight.js
@@ -1,7 +1,17 @@
+function myFunction() {
+   document.getElementById("left").style.backgroundColor="white";
+   document.getElementById("body").style.backgroundColor="black";
+   document.getElementById("right").style.backgroundColor="black";
+   document.getElementById("slider").innerHTML="OFF";
+   document.getElementById("slider").style.color="white";
+   document.getElementById("left").style.cssFloat="right";
+}
 
-function myFunction()
-{
-	document.getElementById("slider").style.cssFloat="right";
-
-	
+function myFunction2() {
+	document.getElementById("left").style.backgroundColor="black";
+   document.getElementById("body").style.backgroundColor="white";
+   document.getElementById("right").style.backgroundColor="white";
+    document.getElementById("slider").innerHTML="ON"
+	 document.getElementById("slider").style.color="black";
+	 document.getElementById("left").style.cssFloat="left";
 }
