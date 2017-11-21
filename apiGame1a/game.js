ff2b57d80e58dd9da9460891d9195ed66fd37d40
JPark954 <31904700+JPark954@users.noreply.github.com>
2017-11-21 18:43:22 -0500

    ap1game startup

---
+++ apiGame1a/game.js
@@ -0,0 +1,24 @@
+"use strict";
+
+
+function MyGame()
+{
+	var version = "v0.1";
+
+	
+	function setStatus(message)
+	{
+		$("#app>footer").text(message);
+	}
+
+	this.start = function()
+	{
+		$("#app>header").append(version);
+		setStatus("Ready");
+	};
+} 
+
+$(function() {
+	window.app = new MyGame();
+	window.app.start();
+});
