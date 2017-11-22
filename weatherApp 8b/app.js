69cc65205442b98dce68f931e355cdd1b10a7f67
JPark954 <31904700+JPark954@users.noreply.github.com>
2017-11-21 19:33:35 -0500

    8b

---
+++ weatherApp 8b/app.js
@@ -0,0 +1,47 @@
+"use strict";
+
+
+function MyWeather()
+{
+	var weatherWidget = new WeatherWidget($("#weather-widget")),
+	version = "8.1";
+
+	// creating a private function
+	function setStatus()
+	{
+		$("#weather-widget").fadeIn();
+		weatherWidget.update();
+	}
+
+	// creating a public function
+	this.start = function()
+	{
+		$("#app>header").append(version);
+		$("#getWeather").click(setStatus);
+	};
+	
+	function getLocation()
+	{
+		if (navigator.geoLocation)
+		{
+			navigator.geoLocation.getCurrentPosition(
+			function(position)
+			{
+				$("#latitude").val(position.coords.latitude);
+				$("#longitude").val(position.coords.longitude);
+			},
+			function(error)
+			{
+				$("#controls .error")
+					.text("ERROR: " + error.message)
+					.slideDown();
+			});
+		}
+	}	
+}
+
+
+$(function() {
+	window.app = new MyWeather();
+	window.app.start();
+});
